e28d09fc8b12e59a402f24df9d74ade6139f2f9b
Luiz Borba <lborba@gmail.com>
2023-04-09 00:03:51 -0300

    Add lightline

--- nvim/lua/modules/theme.lua
+++ nvim/lua/modules/theme.lua
@@ -1,17 +1,16 @@
 function setup()
-  local status, tokyonight = pcall(require, 'tokyonight')
+  require('tokyonight').setup {
+    style = 'storm'
+  }
 
-  if status then
-    tokyonight.setup {
-      style = "storm"
-    }
-    vim.cmd[[colorscheme tokyonight]]
-  end
+  vim.cmd[[colorscheme tokyonight]]
 end
 
 return {
   'folke/tokyonight.nvim',
   lazy = false,
   priority = 1000,
-  config = setup
+  config = setup,
+
+  'itchyny/lightline.vim'
 }
